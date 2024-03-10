24ff0827d01f23de5f3d656563133d816b8bdc9e
Priyanka <Priyanka@Priyanka-PC>
2024-03-10 19:47:25 +0530

    java program

---
+++ Javaproject/src/Variables/Test.java
@@ -0,0 +1,18 @@
+package Variables;
+
+public class Test {
+	public void puppyAge() {
+		 int age =0;
+		 age= age+7;
+		 System.out.println("puppyAge is : " +age);
+		 
+		 
+	}
+
+	public static void main(String[] args) {
+		Test test =new Test();
+		test.puppyAge();
+
+	}
+
+}
