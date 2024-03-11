6c1d68b2315a8779881cc1c9eee9248c474db020
Priyanka <Priyanka@Priyanka-PC>
2024-03-11 14:38:09 +0530

    java code

---
+++ Javaproject/src/Decisionmaking/Ifelseif.java
@@ -0,0 +1,25 @@
+package Decisionmaking;
+
+public class Ifelseif {
+
+	public static void main(String[] args) {
+		int x=30;
+		if(x==10) {
+			System.out.println("value of x is 10");
+		}
+		else if(x==20){
+			System.out.println("value of x is 20");
+			
+			
+		}
+		else if(x==15) {
+			System.out.println("value of x is 15");
+		}
+		else {
+			System.out.println("this is else statement");
+			
+		}
+
+	}
+
+}
