24a3fa259edd3a659dfea6ee8f6d987eb3830df3
Priyanka <Priyanka@Priyanka-PC>
2024-03-11 14:24:09 +0530

    java code

---
+++ Javaproject/src/Decisionmaking/Ifelse.java
@@ -0,0 +1,16 @@
+package Decisionmaking;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		int x=30;
+		if(x<20) {
+			System.out.println("This is if statement");
+		}
+			else {
+				System.out.println("This is else statement");
+			}
+
+	}
+
+}
