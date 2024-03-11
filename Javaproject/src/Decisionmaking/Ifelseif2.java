1f1bc6c77b690f1e36d13d840813024371783a80
Priyanka <Priyanka@Priyanka-PC>
2024-03-11 14:45:58 +0530

    java code

---
+++ Javaproject/src/Decisionmaking/Ifelseif2.java
@@ -0,0 +1,19 @@
+package Decisionmaking;
+
+public class Ifelseif2 {
+
+	public static void main(String[] args) {
+
+		int marks=80;
+		if (marks>80) {
+			System.out.println("Topper");
+		}
+		else if(marks<80 && marks>=60) {
+			System.out.println("first");
+		}
+		else {
+			System.out.println("second");
+		}
+	}
+
+}
