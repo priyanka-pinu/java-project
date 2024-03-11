2fe42e06fc716168c6b1bb08d62a92b2612612c7
Priyanka <Priyanka@Priyanka-PC>
2024-03-11 15:01:35 +0530

    java code

---
+++ Javaproject/src/Decisionmaking/Nestedifelse.java
@@ -0,0 +1,27 @@
+package Decisionmaking;
+
+public class Nestedifelse {
+
+	public static void main(String[] args) {
+
+		int a=10; int b=20;int c=30;
+		if(a>b) {
+			if (a>c) {
+				System.out.println("a");
+			}
+			else {
+				System.out.println("c");
+			}
+		}
+		else{
+			if(b>c) {
+				System.out.println("b");
+				
+			}
+			else {
+				System.out.println("c is greater than b");
+			}
+		}
+	}
+
+}
