5ea3bd91ba2fd05382ed658572ed57b8f344206b
Priyanka <Priyanka@Priyanka-PC>
2024-03-16 15:38:08 +0530

    java code

---
+++ Javaproject/src/Break/Example1.java
@@ -0,0 +1,19 @@
+package Break;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		 int x = 10;
+
+	      while( true ) {
+	         System.out.print("value of x : " + x );
+	         x++;
+	         if(x == 15) {
+	            break;
+	         }
+	         System.out.print("\n");
+
+	}
+
+}
+}
