70e91af737d8fe84b6396297e30f8071447aea2f
Priyanka <Priyanka@Priyanka-PC>
2024-03-16 15:43:20 +0530

    java

---
+++ Javaproject/src/Continue/Example1.java
@@ -0,0 +1,19 @@
+package Continue;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		  int x = 10;
+
+	      while( x < 20 ) {
+	         x++;
+	         if(x == 15){
+	            continue;		 
+	         }   
+	         System.out.print("value of x : " + x );
+	         System.out.print("\n");
+	      }
+	   }
+	}
+
+
